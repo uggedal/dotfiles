51492d7eda1b59c0b9bbb74536ca3d18afa47400
Eivind Uggedal <eivind@uggedal.com>
2013-04-28 22:30:47 +0200

    Setup vim as editor for fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,15 @@
+# Helpers
+function _which
+  which "$argv[1]" 2>/dev/null;
+end
+
 # Local bin dir
 if test -d "$HOME/bin"
   set -gx PATH "$HOME/bin" $PATH
+end
+
+# Editor
+if _which vim
+  set -gx EDITOR (which vim)
+  set -gx VISUAL "$EDITOR"
 end
