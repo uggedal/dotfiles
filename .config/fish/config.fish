f1b7a9fe3db56195b4ee889f0bf4ae97581b8d9b
Eivind Uggedal <eivind@uggedal.com>
2013-04-29 11:11:24 +0200

    Restructure fish conf.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,33 @@
+#
 # Helpers
+#
+
 function _which
   which "$argv[1]" >- ^-;
 end
 
-# Local bin dir
+
+#
+# Env
+#
+
+
 if test -d "$HOME/bin"
   set -gx PATH "$HOME/bin" $PATH
 end
 
-# Editor
 if _which vim
   set -gx EDITOR (which vim)
   set -gx VISUAL "$EDITOR"
 end
 
-# Browser
 if _which chromium
   set -gx BROWSER (which chromium)
 end
 
+
+#
 # Aliases
+#
+
 function ls; ls -hF --color=auto $argv; end
