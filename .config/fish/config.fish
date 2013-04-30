108d0d06ff504c4f59980c760a0fc28ef66fcb4b
Eivind Uggedal <eivind@uggedal.com>
2013-04-30 22:44:47 +0200

    Fish: right prompt if non-zero return code.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -40,10 +40,19 @@ function ls; command ls -hF --color=auto $argv; end
 #
 
 function fish_prompt
-
   if test -n "$SSH_CONNECTION"
     printf '%s ' (hostname)
   end
 
   printf '%s ' (prompt_pwd)
+end
+
+function fish_right_prompt
+  set -l last_status $status
+
+  if test $last_status -ne 0
+    set_color red
+    printf '%d ' $last_status
+    set_color normal
+  end
 end
