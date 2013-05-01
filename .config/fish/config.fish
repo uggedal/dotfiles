291f384626e99b52d97e118c0cdc1293d3bf89bb
Eivind Uggedal <eivind@uggedal.com>
2013-05-01 22:38:49 +0200

    Change grep color.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,6 +35,9 @@ if _which less
   set -gx LESS_TERMCAP_me (printf \e\[0m)         # end all modes
 end
 
+# Fish itself aliases grep to grep --color=auto
+set -gx GREP_COLOR '1;32'
+
 if _which chromium
   set -gx BROWSER (which chromium)
 end
