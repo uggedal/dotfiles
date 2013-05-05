6c16815ca20c6ed4a2df5f2c70579765f25425ad
Eivind Uggedal <eivind@uggedal.com>
2013-05-05 11:20:50 +0200

    Fish: doc ls alias.

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,3 +1,5 @@
+# Use human readable format for file sizes and append indicators to
+# directories (/), executables (*), symlinks (@), sockets (=), and FIFOs (|).
 function ls
   command ls -hF --color=auto $argv
 end
