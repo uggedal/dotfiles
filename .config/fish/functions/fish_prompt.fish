bd6587d58b24437083e9d4b874aaf5a9a3ae3c2e
Eivind Uggedal <eivind@uggedal.com>
2013-05-10 11:33:19 +0200

    Show active virtualenv in prompt.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,10 @@
 function fish_prompt
+  if test -n "$VIRTUAL_ENV"
+    set_color green
+    printf '%s ' (basename $VIRTUAL_ENV)
+    set_color normal
+  end
+
   if test -n "$SSH_CONNECTION"
     printf '%s ' $HOSTNAME
   end
