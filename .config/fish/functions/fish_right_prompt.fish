69fb7fcd121671accba5c4c2f31a545cf9a8b0b4
Eivind Uggedal <eivind@uggedal.com>
2013-05-11 12:21:45 +0200

    Fish: use var for prompt error color.

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -2,7 +2,7 @@ function fish_right_prompt
   set -l last_status $status
 
   if test $last_status -ne 0
-    set_color red
+    set_color $__fish_color_prompt_error
     printf '%d' $last_status
     set_color normal
   end
