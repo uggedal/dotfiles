dd2e33e5551e1b3de739bd6db52ebde80655d0f2
Eivind Uggedal <eivind@uggedal.com>
2013-05-13 16:21:06 +0200

    Bash: custom virtualenv marker in prompt.

--- .bash.d/prompt.sh
+++ .bash.d/prompt.sh
@@ -1,11 +1,12 @@
+C_RED=$'\033[0;31m'
+C_GREEN=$'\033[0;32m'
+C_GREY=$'\033[1;34m'
+C_RESET=$'\033[m'
+
 # Only show hostname when connected over ssh:
 if [ -n "$SSH_CONNECTION" ]; then
   HOSTNAME_SYMBOL=$(_chemical_element_to_symbol $HOSTNAME)' '
 fi
-
-C_RED=$'\033[0;31m'
-C_GREY=$'\033[1;34m'
-C_RESET=$'\033[m'
 
 # Color prompt symbol red when last command had a non-zero exit code:
 _prompt_symbol() {
@@ -20,4 +21,11 @@ _prompt_symbol() {
   printf $C_RESET
 }
 
-PS1='$HOSTNAME_SYMBOL\w $(_prompt_symbol $?) '
+# Custom virtualenv marker:
+_prompt_virtualenv() {
+  if [ -n "$VIRTUAL_ENV" ]; then
+    printf '%s%s%s ' $C_GREEN $(basename $VIRTUAL_ENV) $C_RESET
+  fi
+}
+
+PS1='$(_prompt_virtualenv)$HOSTNAME_SYMBOL\w $(_prompt_symbol $?) '
