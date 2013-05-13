6e15a8ee2802f747d7dc05ca01d03c17a1a5584a
Eivind Uggedal <eivind@uggedal.com>
2013-05-13 10:22:48 +0200

    Bash: wrap ssh/scp/git with keychain init.

--- .bash.d/keychain.sh
+++ .bash.d/keychain.sh
@@ -1,1 +1,35 @@
-eval $(keychain --quiet --quick --eval)
+# Bail if we don't have keychain on the path:
+command -v keychain >/dev/null || return
+
+keychain_init() {
+  local private_key=$HOME/.ssh/id_rsa
+
+  [ -e $private_key ] || return 1
+  eval $(keychain --quiet --quick --agents ssh --eval $private_key)
+}
+
+if command -v ssh >/dev/null; then
+  ssh() {
+    keychain_init
+    command ssh "$@"
+  }
+fi
+
+if command -v scp >/dev/null; then
+  ssh() {
+    keychain_init
+    command scp "$@"
+  }
+fi
+
+if command -v git >/dev/null; then
+  git() {
+    local network_actions='(push|pull|fetch)'
+
+    if [[ $1 =~ $network_actions ]]; then
+      keychain_init
+    fi
+
+    command git "$@"
+  }
+fi
