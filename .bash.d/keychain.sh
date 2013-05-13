2965099e2fdabf155e54b85d76131a28f1879fac
Eivind Uggedal <eivind@uggedal.com>
2013-05-13 11:31:04 +0200

    Bash: prefix private keychain fn with underscore.

--- .bash.d/keychain.sh
+++ .bash.d/keychain.sh
@@ -1,7 +1,7 @@
 # Bail if we don't have keychain on the path:
 command -v keychain >/dev/null || return
 
-keychain_init() {
+_keychain_init() {
   local private_key=$HOME/.ssh/id_rsa
 
   [ -e $private_key ] || return 1
@@ -10,14 +10,14 @@ keychain_init() {
 
 if command -v ssh >/dev/null; then
   ssh() {
-    keychain_init
+    _keychain_init
     command ssh "$@"
   }
 fi
 
 if command -v scp >/dev/null; then
   ssh() {
-    keychain_init
+    _keychain_init
     command scp "$@"
   }
 fi
@@ -27,7 +27,7 @@ if command -v git >/dev/null; then
     local network_actions='(push|pull|fetch)'
 
     if [[ $1 =~ $network_actions ]]; then
-      keychain_init
+      _keychain_init
     fi
 
     command git "$@"
