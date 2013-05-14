d71912f50d0a010816c04ffef93a1a8ff0c959dc
Eivind Uggedal <eivind@uggedal.com>
2013-05-14 11:30:12 +0200

    Bash: fix prompt escaping.

--- .bash.d/prompt.bash
+++ .bash.d/prompt.bash
@@ -1,31 +1,33 @@
-C_RED=$'\033[0;31m'
-C_GREEN=$'\033[0;32m'
-C_GREY=$'\033[1;34m'
-C_RESET=$'\033[m'
+C_RED='\[\e[31m\]'
+C_GREEN='\[\e[32m\]'
+C_GREY='\[\e[1;34m\]'
+C_RESET='\[\e[0m\]'
 
-# Only show hostname when connected over ssh:
+PS1=''
+
+# Show hostname when connected over ssh:
 if [ -n "$SSH_CONNECTION" ]; then
-  HOSTNAME_SYMBOL=$(_chemical_element_to_symbol $HOSTNAME)' '
+  PS1=$(_chemical_element_to_symbol $HOSTNAME)' '
 fi
 
-# Color prompt symbol red when last command had a non-zero exit code:
-_prompt_symbol() {
-  local last_status=$1
+# Show $CWD:
+PS1="$PS1"'\w '
 
-  if [ $last_status -eq 0 ]; then
-    printf $C_GREY
-  else
-    printf $C_RED
-  fi
-  printf '❯'
-  printf $C_RESET
+# Color prompt symbol red when last command had a non-zero exit code:
+_ok_status() {
+  [ $1 -eq 0 -o $1 -eq 130 ]
 }
+PS1="$PS1"'$(_ok_status $? && printf "'${C_GREY}'" || printf "'${C_RED}'")'
+PS1="${PS1}❯${C_RESET} "
+
 
 # Custom virtualenv marker:
-_prompt_virtualenv() {
-  if [ -n "$VIRTUAL_ENV" ]; then
-    printf '%s%s%s ' $C_GREEN $(basename $VIRTUAL_ENV) $C_RESET
-  fi
-}
+if command -v virtualenv >/dev/null; then
+  _prompt_virtualenv() {
+    if [ -n "$VIRTUAL_ENV" ]; then
+      printf $(basename $VIRTUAL_ENV)
+    fi
+  }
 
-PS1='$(_prompt_virtualenv)$HOSTNAME_SYMBOL\w $(_prompt_symbol $?) '
+  PS1='${C_GREEN}$(_prompt_virtualenv)${C_RESET} '"$PS1"
+fi
