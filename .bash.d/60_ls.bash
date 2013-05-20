6376fcb9c51a8604483630c96682510c9769c79b
Eivind Uggedal <eivind@uggedal.com>
2013-05-20 11:24:41 +0200

    Bash: make ls list directories before files.

--- .bash.d/60_ls.bash
+++ .bash.d/60_ls.bash
@@ -1,10 +1,12 @@
 # Use ls with:
+#   - list directories before files
 #   - human readable format for file sizes
 #   - append indicators to directories (/), executables (*), symlinks (@),
 #     sockets (=), and FIFOs (|)
 #   - list dates for long listings in full iso format
 
 alias ls='command ls \
+  --group-directories-first \
   --human-readable \
   --classify \
   --time-style=long-iso \
