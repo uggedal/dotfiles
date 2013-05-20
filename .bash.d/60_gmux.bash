c3ae94295a4fb342d4aaedf78ff6ac25037b2fa2
Eivind Uggedal <eivind@uggedal.com>
2013-05-20 14:46:18 +0200

    Bash: function for adjusting gmux brightness.

---
+++ .bash.d/60_gmux.bash
@@ -0,0 +1,18 @@
+# Function for adjusting the display brightness on Macs with a gmux
+
+[ -d /sys/class/backlight/gmux_backlight ] || return
+command -v bc >/dev/null || return
+
+gmux() {
+  local gmux=/sys/class/backlight/gmux_backlight
+  local cur=$gmux/brightness
+  local max=$gmux/max_brightness
+  local elevator='su root -c'
+
+  if [ $# -eq 0 ]; then
+    echo $(printf 'scale=0; (%s * 100) / %s\n' $(<$cur) $(<$max) | bc)\%
+  elif [ $# -eq 1 ]; then
+    command -v sudo >/dev/null && elevator='sudo'
+    $elevator "printf 'scale=0; (%s / 100) * %s\n' $(<$max) $1 | bc > $cur"
+  fi
+}
