39bc5e0266ccd8fcb517ec06db399778fa291dc2
Eivind Uggedal <eivind@uggedal.com>
2013-05-23 17:46:06 +0200

    Bash: make mplayer wrapper a function.

---
+++ .bash.d/60_mplayer.bash
@@ -0,0 +1,14 @@
+command -v mplayer >/dev/null || return
+
+mplayer() {
+  local opt=""
+
+  if command -v vainfo >/dev/null && vainfo >/dev/null; then
+    opt="-vo vaapi"
+  fi
+
+  case $1 in
+    *.rar) 7z x -so $1 2>/dev/null | /usr/bin/mplayer $opt -;;
+    *) /usr/bin/mplayer $opt "$@";;
+  esac
+}
