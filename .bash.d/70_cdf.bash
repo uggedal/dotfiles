7c81c487e0934f00a2d7cc0bb1f81a41c8b20ea0
Eivind Uggedal <eivind@uggedal.com>
2013-06-05 15:25:57 +0200

    Bash: fix optimization of cdf helper.

--- .bash.d/70_cdf.bash
+++ .bash.d/70_cdf.bash
@@ -1,7 +1,7 @@
 # Find subdirectory with the given name and cd to it.
 cdf() {
 
-  local d=$(find . -name "$1" -type d -quit)
+  local d=$(find . -name "$1" -type d -print -quit)
 
   [ -n "$d" ] && cd "$d"
 }
