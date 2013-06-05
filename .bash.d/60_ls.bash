5ba8f5a9974864a35870acf6e1333eecfe17ada6
Eivind Uggedal <eivind@uggedal.com>
2013-06-05 16:04:22 +0200

    Bash: cygwin does not handle line continuation in single quotes.

--- .bash.d/60_ls.bash
+++ .bash.d/60_ls.bash
@@ -6,10 +6,10 @@
 #     sockets (=), and FIFOs (|)
 #   - list dates for long listings in full iso format
 
-alias ls='command ls \
+alias ls="command ls \
   -1 \
   --group-directories-first \
   --human-readable \
   --classify \
   --time-style=long-iso \
-  --color=auto'
+  --color=auto"
