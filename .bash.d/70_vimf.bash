0de4e158d1aa3d2960e32e54473e69c0a8abe02b
Eivind Uggedal <eivind@uggedal.com>
2013-06-06 09:19:20 +0200

    Bash: make vimf take optional context dir.

--- .bash.d/70_vimf.bash
+++ .bash.d/70_vimf.bash
@@ -1,7 +1,18 @@
-# Find file with the given name and edit it.
+# Find file with the given name and optional context dir and edit it.
 vimf() {
+  local d='.'
 
-  local f=$(find . -name "$1" -type f -print -quit)
+  if [ $# -ne 1 -a $# -ne 2 ]; then
+    printf 'Usage: %s [context] <file>\n' $(basename $0) >&2
+    return 64
+  fi
+
+  if [ $# -eq 2 ]; then
+    d=$1
+    shift;
+  fi
+
+  local f=$(find $d -name "$1" -type f -print -quit)
 
   [ -n "$f" ] && $EDITOR "$f"
 }
