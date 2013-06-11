1a8aaed288df6befcc780f112ce55e35de6b7461
Eivind Uggedal <eivind@uggedal.com>
2013-06-11 09:09:00 +0200

    Bash: better ls feature detection.

--- .bash.d/60_ls.bash
+++ .bash.d/60_ls.bash
@@ -6,13 +6,14 @@
 #     sockets (=), and FIFOs (|)
 #   - list dates for long listings in full iso format
 
-# Check for newer capabilities:
-command ls --group-directories-first / &>/dev/null || return
+ls_flags='-1 --human-readable --classify --time-style=long-iso --color=auto'
 
-alias ls="command ls \
-  -1 \
-  --group-directories-first \
-  --human-readable \
-  --classify \
-  --time-style=long-iso \
-  --color=auto"
+# ls --group-directories-first was introduced in coreutils 6.0 (2006-08-15).
+# Since RHEL 5.x uses 5.97 we need to check for it:
+if command ls --group-directories-first / &>/dev/null; then
+  ls_flags="$ls_flags --group-directories-first"
+fi
+
+alias ls="command ls $ls_flags"
+
+unset ls_flags
