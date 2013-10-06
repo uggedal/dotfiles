20394c394bfeb74f70de54120a59654b497d72b8
Eivind Uggedal <eivind@uggedal.com>
2013-10-06 23:39:30 +0200

    bash: kill prompt symbol

--- .bash.d/10_prompt.bash
+++ .bash.d/10_prompt.bash
@@ -11,27 +11,14 @@ if [ -n "$SSH_CONNECTION" ]; then
   PS1=$(_chemical_element_to_symbol $HOSTNAME)' '
 fi
 
-# Show $CWD:
-PS1="$PS1"'\w '
-
-# Use unicode prompt symbol in when:
-#   - running with UTF-8 locale and
-#   - not in real console where fonts are limited to ASCII
-if tty | grep -F /dev/pts >/dev/null && [[ $LANG == *UTF-8* ]]; then
-  prompt_symbol='❯'
-else
-  prompt_symbol='>'
-fi
-
 # Color prompt symbol red when last command had a non-zero exit code:
 _ok_status() {
   local rc=$_prompt_status
   unset _prompt_status
   [ $rc -eq 0 -o $rc -eq 130 ]
 }
-PS1="$PS1"'$(_ok_status || printf "'${C_RED}'")'
-PS1="${PS1}${prompt_symbol}${C_RESET} "
+PS1="$PS1"'$(_ok_status || printf "'${C_RED}'")\w'"${C_RESET} "
 
-PS2="${C_BROWN}${prompt_symbol}${C_RESET} "
+PS2="${C_BROWN}>${C_RESET} "
 
 unset prompt_symbol
