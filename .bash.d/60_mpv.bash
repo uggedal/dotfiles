0ad7a32d718e60198b89372b7064f22d9258e85e
Eivind Uggedal <eivind@uggedal.com>
2014-02-03 12:38:56 +0000

    mpv: streaming rar support

--- .bash.d/60_mpv.bash
+++ .bash.d/60_mpv.bash
@@ -1,5 +1,5 @@
 # Wrapper around mpv which enables vaapi or vdpau if we're on a machine
-# with support.
+# with support. Also handles streaming content from RAR files.
 
 command -v mpv >/dev/null || return
 
@@ -13,5 +13,8 @@ mpv() {
     flags='--vo vaapi --hwdec vaapi'
   fi
 
-  /usr/bin/mpv $flags "$@"
+  case $1 in
+    *.rar) unrar p -inul $1 2>/dev/null | /usr/bin/mpv $flags "$@" -;;
+    *) /usr/bin/mpv $flags "$@";;
+  esac
 }
