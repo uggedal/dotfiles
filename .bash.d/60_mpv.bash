a97ec2e6700cf2e4b8d6068742f7c1a0f436a567
Eivind Uggedal <eivind@uggedal.com>
2014-02-08 19:24:47 +0100

    mpv: disable vdpau sniffing

--- .bash.d/60_mpv.bash
+++ .bash.d/60_mpv.bash
@@ -5,13 +5,7 @@ command -v mpv >/dev/null || return
 
 mpv() {
   local xlog=/var/log/Xorg.0.log
-  local flags=
-
-  if test -f $xlog && grep -q 'VDPAU driver' $xlog; then
-    flags='--vo vdpau --hwdec vdpau'
-  elif command -v vainfo >/dev/null && vainfo &>/dev/null; then
-    flags='--vo vaapi --hwdec vaapi'
-  fi
+  local flags='--vo vaapi --hwdec vaapi'
 
   case "$1" in
     *.rar) unrar p -inul "$1" 2>/dev/null | /usr/bin/mpv $flags "$@" -;;
