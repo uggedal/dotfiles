6d0410a7b8a4e17d1922108910525a7abd16b75d
Eivind Uggedal <eivind@uggedal.com>
2014-07-08 07:08:03 +0000

    mpv: no vdpau support

--- .bash.d/60_mpv.bash
+++ .bash.d/60_mpv.bash
@@ -1,10 +1,8 @@
-# Wrapper around mpv which enables vaapi or vdpau if we're on a machine
-# with support. Also handles streaming content from RAR files.
+# Wrapper around mpv which enables streaming of content from RAR files.
 
 command -v mpv >/dev/null || return
 
 mpv() {
-  local xlog=/var/log/Xorg.0.log
   local flags='--vo vaapi --hwdec vaapi'
 
   case "$1" in
