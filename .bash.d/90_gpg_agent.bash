49060e02d603c61c26f04f89e88eba77efa7256d
Eivind Uggedal <eivind@uggedal.com>
2014-10-21 22:24:05 +0200

    gpg: actually export agent vars

--- .bash.d/90_gpg_agent.bash
+++ .bash.d/90_gpg_agent.bash
@@ -6,9 +6,10 @@ agent_info=$XDG_RUNTIME_DIR/gpg-agent-info
 
 if [ -f $agent_info ] && kill -0 $(head -n1 $agent_info | cut -d: -f2) 2>/dev/null; then
   . $agent_info
-  export GPG_AGENT_INFO SSH_AUTH_SOCK
 else
   eval $(gpg-agent --daemon --enable-ssh-support --write-env-file $agent_info)
 fi
+
+export GPG_AGENT_INFO SSH_AUTH_SOCK SSH_AGENT_PID
 
 unset agent_info
