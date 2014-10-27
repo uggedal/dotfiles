20557118af90b68e679c1dd1a33b8e02915999e2
Eivind Uggedal <eivind@uggedal.com>
2014-10-27 23:13:14 +0100

    Revert "gpg: rm agent hack"
    This reverts commit 0d521777f6a0627d09f6fa4b8306bc2369e6c3da.

---
+++ .bash.d/90_gpg_agent.bash
@@ -0,0 +1,24 @@
+# GPG agent
+
+command -v gpg-agent >/dev/null || return
+
+case $(tty) in
+  /dev/tty*)
+    return
+    ;;
+esac
+
+GPG_TTY=$(tty)
+export GPG_TTY
+
+agent_info=$HOME/.cache/gpg-agent-info
+
+if [ -f $agent_info ] && kill -0 $(head -n1 $agent_info | cut -d: -f2) 2>/dev/null; then
+  . $agent_info
+else
+  eval $(gpg-agent --daemon --enable-ssh-support --write-env-file $agent_info)
+fi
+
+export GPG_AGENT_INFO SSH_AUTH_SOCK SSH_AGENT_PID
+
+unset agent_info
