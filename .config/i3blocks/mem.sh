3c9fc469c6934d6b4a2854ccfcb3d73823499d0b
Eivind Uggedal <eivind@uggedal.com>
2014-11-11 10:32:00 +0100

    i3: mem

---
+++ .config/i3blocks/mem.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+. $(dirname $0)/colors.sh
+
+color() {
+  local perc=$1
+  local c=$RED
+
+  [ "$perc" -lt 75 ] && c=$ORANGE
+  [ "$perc" -lt 50 ] && c=$YELLOW
+  [ "$perc" -lt 25 ] && c=$GREEN
+
+  printf '%s' $c
+}
+
+_mem() {
+  awk '
+/^MemTotal:/ {
+  total=$2
+}
+/^MemFree:/ {
+  free=$2
+}
+/^Buffers:/ {
+  buffers=$2
+}
+/^Cached:/ {
+  cached=$2
+}
+END {
+  unused=free+buffers+cached
+  printf "%dM %.0f", (unused/1024), ((total-unused)/total*100)
+}
+' /proc/meminfo
+}
+
+status() {
+  local stats="$(_mem)"
+
+  local free=${stats% *}
+  local used=${stats#* }
+
+  echo $used 1>&2
+
+  printf -- ' %s \n\n%s\n' $free $(color $used)
+}
+
+status "$@"
