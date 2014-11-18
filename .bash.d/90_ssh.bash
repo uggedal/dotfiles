0475db53430e6e9feb538c147276eb104006b008
Eivind Uggedal <eivind@uggedal.com>
2014-11-18 08:42:37 +0000

    ssh: fix ssh auth update

---
+++ .bash.d/90_ssh.bash
@@ -0,0 +1,54 @@
+
+_ssh_agent() {
+  local agent_info=$HOME/.cache/ssh-agent-info
+
+  if [ -f $agent_info ] && . $agent_info >/dev/null && kill -0 $SSH_AGENT_PID 2>/dev/null; then
+    :
+  else
+    ssh-agent > $agent_info
+    . $agent_info >/dev/null
+  fi
+}
+
+command -v ssh-agent >/dev/null && [ -z "$SSH_CONNECTION" ] && _ssh_agent
+
+_tmux_update_ssh_auth_sock() {
+  [ -n "$TMUX" ] || return
+
+  local sock=$(tmux showenv | grep '^SSH_AUTH_SOCK' | cut -d= -f2)
+
+  [ "$SSH_AUTH_SOCK" = "$sock" ] && return
+
+  if [ -S "$sock" ]; then
+    SSH_AUTH_SOCK=$sock
+  fi
+}
+
+_wrap_ssh() {
+  if ! ssh-add -l >/dev/null; then
+    ssh-add $(grep -l 'ENCRYPTED$' .ssh/*id_rsa)
+  fi
+}
+
+ssh() {
+  _wrap_ssh "$@"
+  _tmux_update_ssh_auth_sock
+  command ssh "$@"
+}
+
+scp() {
+  _wrap_ssh "$@"
+  _tmux_update_ssh_auth_sock
+  command scp "$@"
+}
+
+git() {
+  case $1 in
+    push|pull|fetch)
+      _tmux_update_ssh_auth_sock
+      _wrap_ssh git "$@"
+      ;;
+  esac
+
+  command git "$@"
+}
