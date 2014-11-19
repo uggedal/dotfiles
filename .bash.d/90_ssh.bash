542dcadca66872c6c33be5c7350b6706e56037c4
Eivind Uggedal <eivind@uggedal.com>
2014-11-19 13:27:43 +0000

    ssh: fix wrapper

--- .bash.d/90_ssh.bash
+++ .bash.d/90_ssh.bash
@@ -1,5 +1,7 @@
-
 _ssh_agent() {
+  command -v ssh-agent >/dev/null || return
+  [ "$SSH_CONNECTION" ] && return
+
   local agent_info=$HOME/.cache/ssh-agent-info
 
   if [ -f $agent_info ] && . $agent_info >/dev/null && kill -0 $SSH_AGENT_PID 2>/dev/null; then
@@ -10,7 +12,7 @@ _ssh_agent() {
   fi
 }
 
-command -v ssh-agent >/dev/null && [ -z "$SSH_CONNECTION" ] && _ssh_agent
+_ssh_agent
 
 _tmux_update_ssh_auth_sock() {
   [ -n "$TMUX" ] || return
@@ -25,6 +27,8 @@ _tmux_update_ssh_auth_sock() {
 }
 
 _wrap_ssh() {
+  [ "$SSH_CONNECTION" ] && return
+
   if ! ssh-add -l >/dev/null; then
     ssh-add $(grep -l 'ENCRYPTED$' .ssh/*id_rsa)
   fi
